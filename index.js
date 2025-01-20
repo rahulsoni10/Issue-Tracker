02d8dce69e47df309cf9abc23f51f387d184e70e
rahulsoni10 <rahulrudra12345@gmail.com>
2025-01-20 15:32:00 +0530

    new commit

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://rahulrudra:rahulrudra@cluster0.z8yh46g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
